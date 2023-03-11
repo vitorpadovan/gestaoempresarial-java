957180450bcf744f969ef919842f6cffc60ba64b
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2023-03-11 13:07:24 -0300

    Commit inicial

---
+++ src/main/java/br/com/compsemdor/GestaoEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package br.com.compsemdor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEmpresarialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compsemdor/GestaoEmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compsemdor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
